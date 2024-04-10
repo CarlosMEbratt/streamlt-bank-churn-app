55bdada8c625de688b9582fe0c218edce4332c0a
Prescila Mora <prescilafmora@gmail.com>
2024-04-10 12:36:12 -0400

    updated the file now upload

--- main.py
+++ main.py
@@ -145,7 +145,7 @@ def form_content(username):
     st.header('Input data')
 
     st.markdown('**1. Use custom data**')
-    uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
+    uploaded_file = st.file_uploader("Upload a CSV file now", type=["csv"])
     if uploaded_file is not None:
         #df = pd.read_csv(uploaded_file, index_col=False)
         df = pd.read_csv(secret_name_or_arn="arn:aws:secretsmanager:us-east-2:767397996410:secret:dev/s3/bucket_token-6t6xMP", index_col=False)
