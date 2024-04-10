a80769cbdce3d22c6adc00a27ab3346b2ad7393a
Carlos Ebratt <carlosmebratt@gmail.com>
2024-04-09 23:36:30 -0400

    a

--- main.py
+++ main.py
@@ -41,17 +41,21 @@ def update_succesful_login(succesful_login):
 
 def login_app():
     db = connect_db()
-    credentials_db = db["credentials"]
+    credentials_db=db["credentials"]
 
+    
     # Initialize Session States.
     if 'username' not in st.session_state:
         st.session_state.username = ''
-    if 'succesful_login' not in st.session_state:
-        st.session_state.succesful_login = False
+    if 'form' not in st.session_state:
+        st.session_state.form = ''
 
-    # Check if the user is already logged in
+    if 'succesful_login' not in st.session_state:
+        st.session_state.succesful_login = False    
+            
     if st.session_state.username != '':
         st.sidebar.markdown(''':blue[You are logged in]''')
+        
         logout = st.sidebar.button(label='Log Out')
         if logout:
             # Handle Logout Click
@@ -60,28 +64,68 @@ def login_app():
             st.session_state.form = ''  # Reset form state
             st.sidebar.success("You have successfully logged out!")
 
-    else:
-        # Render sign-in form and button
+    
+
+    # Initialize Sing In or Sign Up forms
+    if st.session_state.form == 'signup_form' and st.session_state.username == '':
+
+    
+        signup_form = st.sidebar.form(key='signup_form', clear_on_submit=True)
+        new_username = signup_form.text_input(label='Enter Username*')
+        new_user_email = signup_form.text_input(label='Enter Email Address*')
+        new_user_pas = signup_form.text_input(label='Enter Password*', type='password')
+        user_pas_conf = signup_form.text_input(label='Confirm Password*', type='password')
+        note = signup_form.markdown('**required fields*')
+        signup = signup_form.form_submit_button(label='Sign Up')
+        
+        if signup:
+            if '' in [new_username, new_user_email, new_user_pas]:
+                st.sidebar.error('Some fields are missing')
+            else:
+                if credentials_db.find_one({'username' : new_username}):
+                    st.sidebar.error('This username already exists')
+                if credentials_db.find_one({'email' : new_user_email}):
+                    st.sidebar.error('This e-mail is already registered')
+                else:
+                    if new_user_pas != user_pas_conf:
+                        st.sidebar.error('Passwords do not match')
+                    else:
+                        user_update(new_username,True)
+                        credentials_db.insert_one({'username' : new_username, 
+                                                'email' : new_user_email, 
+                                                'password' : new_user_pas,
+                                                "creation_time":datetime.now()})
+                        st.sidebar.success('You have successfully registered!')
+                        
+                        login_form = st.sidebar.form(key='signin_form', clear_on_submit=True)
+                        username = login_form.text_input(label='Enter Username')
+                        password = login_form.text_input(label='Enter Password', type='password')
+                        
+                        del new_user_pas, user_pas_conf                        
+    
+    
+    elif st.session_state.username == '':
         login_form = st.sidebar.form(key='signin_form', clear_on_submit=True)
         username = login_form.text_input(label='Enter Username')
-        password = login_form.text_input(label='Enter Password', type='password')
+        password = login_form.text_input(label='Enter Password', type='password')        
+        
 
-        if credentials_db.find_one({'username': username, 'password': password}):
-            login = login_form.form_submit_button(label='Sign In', on_click=user_update(username, True))
+        if credentials_db.find_one({'username' : username, 'password' : password}):
+            login = login_form.form_submit_button(label='Sign In', on_click=user_update(username,True))
             if login:
-                st.sidebar.success(f"You are logged in as {username.upper()}")
+                st.sidebar.success(f"You are logged in as {username.upper()}")  
                 del password
         else:
             login = login_form.form_submit_button(label='Sign In')
             if login:
                 st.sidebar.error("Username or Password is incorrect. Please try again or create an account.")
+        
 
-        # 'Create Account' button
-        signup_request = st.sidebar.button('Create Account', on_click=select_signup)
-
+    # 'Create Account' button
+    if st.session_state.username == "" and st.session_state.form != 'signup_form':
+        signup_request = st.sidebar.button('Create Account', on_click=select_signup)  
+    
     return st.session_state.username, st.session_state.succesful_login
-
-
 
 
 
