51efacd19133ac7f7926655a0dea740c6e697f27
Carlos Ebratt <carlosmebratt@gmail.com>
2024-04-09 23:37:52 -0400

    a

--- main.py
+++ main.py
@@ -32,102 +32,55 @@ def connect_db():
 def select_signup():
     st.session_state.form = 'signup_form'
 
-def user_update(name, succesful_login):
+def user_update(name, successful_login):
     st.session_state.username = name
-    st.session_state.succesful_login=succesful_login
+    st.session_state.successful_login = successful_login
     
-def update_succesful_login(succesful_login):
-    st.session_state.succesful_login=succesful_login
+def update_successful_login(successful_login):
+    st.session_state.successful_login = successful_login
 
 def login_app():
     db = connect_db()
-    credentials_db=db["credentials"]
+    credentials_db = db["credentials"]
 
-    
     # Initialize Session States.
     if 'username' not in st.session_state:
         st.session_state.username = ''
-    if 'form' not in st.session_state:
-        st.session_state.form = ''
+    if 'successful_login' not in st.session_state:
+        st.session_state.successful_login = False
 
-    if 'succesful_login' not in st.session_state:
-        st.session_state.succesful_login = False    
-            
     if st.session_state.username != '':
         st.sidebar.markdown(''':blue[You are logged in]''')
-        
         logout = st.sidebar.button(label='Log Out')
         if logout:
             # Handle Logout Click
             st.session_state.username = ''  # Set username to empty string
-            st.session_state.succesful_login = False  # Set successful_login to False
+            st.session_state.successful_login = False  # Set successful_login to False
             st.session_state.form = ''  # Reset form state
             st.sidebar.success("You have successfully logged out!")
 
-    
-
-    # Initialize Sing In or Sign Up forms
-    if st.session_state.form == 'signup_form' and st.session_state.username == '':
-
-    
-        signup_form = st.sidebar.form(key='signup_form', clear_on_submit=True)
-        new_username = signup_form.text_input(label='Enter Username*')
-        new_user_email = signup_form.text_input(label='Enter Email Address*')
-        new_user_pas = signup_form.text_input(label='Enter Password*', type='password')
-        user_pas_conf = signup_form.text_input(label='Confirm Password*', type='password')
-        note = signup_form.markdown('**required fields*')
-        signup = signup_form.form_submit_button(label='Sign Up')
-        
-        if signup:
-            if '' in [new_username, new_user_email, new_user_pas]:
-                st.sidebar.error('Some fields are missing')
-            else:
-                if credentials_db.find_one({'username' : new_username}):
-                    st.sidebar.error('This username already exists')
-                if credentials_db.find_one({'email' : new_user_email}):
-                    st.sidebar.error('This e-mail is already registered')
-                else:
-                    if new_user_pas != user_pas_conf:
-                        st.sidebar.error('Passwords do not match')
-                    else:
-                        user_update(new_username,True)
-                        credentials_db.insert_one({'username' : new_username, 
-                                                'email' : new_user_email, 
-                                                'password' : new_user_pas,
-                                                "creation_time":datetime.now()})
-                        st.sidebar.success('You have successfully registered!')
-                        
-                        login_form = st.sidebar.form(key='signin_form', clear_on_submit=True)
-                        username = login_form.text_input(label='Enter Username')
-                        password = login_form.text_input(label='Enter Password', type='password')
-                        
-                        del new_user_pas, user_pas_conf                        
-    
-    
-    elif st.session_state.username == '':
-        login_form = st.sidebar.form(key='signin_form', clear_on_submit=True)
+    else:
+        # Render sign-in form and button
+        login_container = st.sidebar.empty()
+        login_container.write("Please sign in.")
+        login_form = login_container.form(key='signin_form', clear_on_submit=True)
         username = login_form.text_input(label='Enter Username')
-        password = login_form.text_input(label='Enter Password', type='password')        
-        
+        password = login_form.text_input(label='Enter Password', type='password')
 
-        if credentials_db.find_one({'username' : username, 'password' : password}):
-            login = login_form.form_submit_button(label='Sign In', on_click=user_update(username,True))
+        if credentials_db.find_one({'username': username, 'password': password}):
+            login = login_form.form_submit_button(label='Sign In', on_click=user_update(username, True))
             if login:
-                st.sidebar.success(f"You are logged in as {username.upper()}")  
+                st.sidebar.success(f"You are logged in as {username.upper()}")
                 del password
         else:
             login = login_form.form_submit_button(label='Sign In')
             if login:
                 st.sidebar.error("Username or Password is incorrect. Please try again or create an account.")
-        
 
-    # 'Create Account' button
-    if st.session_state.username == "" and st.session_state.form != 'signup_form':
-        signup_request = st.sidebar.button('Create Account', on_click=select_signup)  
-    
-    return st.session_state.username, st.session_state.succesful_login
+        # 'Create Account' button
+        signup_request = st.sidebar.button('Create Account', on_click=select_signup)
 
-
+    return st.session_state.username, st.session_state.successful_login
 
 #'''Funcionality------------------------------------------------------------------------------------------------------------- '''
 def form_content(username):
@@ -137,31 +90,16 @@ def form_content(username):
     st.markdown('**1. Use custom data**')
     uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
     if uploaded_file is not None:
-        #df = pd.read_csv(uploaded_file, index_col=False)
-        df = pd.read_csv(secret_name_or_arn="arn:aws:secretsmanager:us-east-2:767397996410:secret:dev/s3/bucket_token-6t6xMP", index_col=False)
-
-        #st.write(df.head())
-        #st.dataframe(data=df, use_container_width=True)
-
+        df = pd.read_csv(uploaded_file)
+        st.dataframe(data=df, use_container_width=True)
 
     # Select example data
     st.markdown('**2. Use example data**')
-
+    st.info("Coming soon...")
 
     # Download example data
-    @st.cache_data
-    def convert_df(input_df):
-        return input_df.to_csv(index=False).encode('utf-8')
-    example_csv = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/delaney_solubility_with_descriptors.csv')
-    csv = convert_df(example_csv)
-    st.download_button(
-        label="Download example CSV",
-        data=csv,
-        file_name='delaney_solubility_with_descriptors.csv',
-        mime='text/csv',
-    )
-
-
+    st.markdown('**3. Download example data**')
+    st.info("Coming soon...")
     
     # Initiate the model building process
     if uploaded_file:  
@@ -182,62 +120,41 @@ def form_content(username):
         with st.spinner('Wait for it...'):
             time.sleep(3)
 
-        #st.write('Customer predictions are now complete!')
         st.markdown(''':blue[Customer predictions are now complete!]''')
 
-        st.dataframe(data=df, use_container_width=True)
-
-
-
-    
-    
-
 #'''Main Function------------------------------------------------------------------------------------------------------------- '''
-    
-
 def main():
     # Initialize Session States.
-    succesful_login=False
-    username, succesful_login=login_app()
+    username, successful_login = login_app()
 
     st.title('Machine Learning App for Bank Churn Prediction')
-    
 
-    if succesful_login == False:        
+    if successful_login == False:
         st.subheader("Please use the sidebar on the left to log in or create an account.")
         st.image('image1.png')
-
     else:
         with st.sidebar:             
             st.header(f"Welcome {username} !")
         
         form_content(username)
 
-
-#'''Sidebar------------------------------------------------------------------------------------------------------------- '''
-
-    
     with st.sidebar:
-
         with st.expander('About this app / Instructions'):
-                    st.markdown('**What can this app do?**')
-                    st.info('This app allow users to load a bank .csv file and use it to build a machine learning model to predict churn. The app also recomends actions to reduce churn.')
+            st.markdown('**What can this app do?**')
+            st.info('This app allows users to load a bank .csv file and use it to build a machine learning model to predict churn. The app also recommends actions to reduce churn.')
 
-                    st.markdown('**How to use the app?**')
-                    st.warning('To engage with the app, please login or create an account and then 1. Select a data set and 2. Click on "Run the model". As a result, this would initiate the ML model and data processing.')
+            st.markdown('**How to use the app?**')
+            st.warning('To engage with the app, please login or create an account and then 1. Select a dataset and 2. Click on "Run the model". As a result, this would initiate the ML model and data processing.')
 
-                    st.markdown('**Under the hood**')
-                    st.markdown('Data sets:')
-                    st.code('''- You can upload your own data set or use the example data set provided in the app.
-                    ''', language='markdown')
-                    
-                    st.markdown('Libraries used:')
-                    st.code('''
-                            * Pandas for data wrangling  
-                            * Scikit-learn
-                            * XGBoost for machine learning
-                            * Streamlit for user interface
-                    ''', language='markdown')
+            st.markdown('**Under the hood**')
+            st.markdown('Data sets:')
+            st.code('''- You can upload your own dataset or use the example dataset provided in the app.''', language='markdown')
+
+            st.markdown('Libraries used:')
+            st.code('''* Pandas for data wrangling
+* Scikit-learn
+* XGBoost for machine learning
+* Streamlit for user interface''', language='markdown')
 
 # Call the main function
 if __name__ == '__main__':
